74397d0432cc8bc2898786e5e56618a1b9f7e1d6
ShayneKaruna <112900215+ShayneKaruna@users.noreply.github.com>
2022-12-22 17:10:20 -0500

    worked on landing page

---
+++ cantam/src/components/HeroImg2.js
@@ -0,0 +1,30 @@
+import "./HeroImgStyles.css";
+
+import React from "react";
+
+import IntroImg from "./images/background.gif";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>Since 2000</p>
+        <h1>CANTAM GROUP</h1>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
