dbb9e8204e0b957e7a7976eb5026c3e6719af2e4
EllisJLC <jia.liang.chen@ryerson.ca>
2022-12-22 21:17:44 -0500

    render page functionality

--- cantam/src/components/Main.js
+++ cantam/src/components/Main.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import HeroImg from "./HeroImg";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
 
 export default function Main() {
-
+  const [currentPage, setCurrentPage] = useState('Landing');
+  const RenderPage = () => {
+    switch (currentPage) {
+      case "Landing":
+        return <HeroImg />
+      case "Homepage":
+        return <Homepage />;
+      default: 
+        return <NotFound />;
+    }
+  }
   return (
     <div >
       <Navbar />
-      <HeroImg />
+      <RenderPage />
     </div>
   );
 }
