12428c9e798a280b40f8990da69dcbcbc17d9802
EllisJLC <jia.liang.chen@ryerson.ca>
2022-12-27 22:52:54 -0500

    added contact page

--- cantam/src/components/pages/Contact.js
+++ cantam/src/components/pages/Contact.js
@@ -1,1 +1,37 @@
+import React, {useState} from "react";
 
+export default function Contact() {
+
+  const styles = {
+  }
+  return (
+    <section>
+      <h1>Contact us!</h1>
+      <p>Find us by:</p>
+      <ul>
+        <li>
+          Email: <a 
+            href="mailto:yaso@cantamgroup.com"
+          >
+            yaso@cantamgroup.com
+          </a>
+        </li>
+        <li>
+          Office: <a
+            href="tel:+14163353353"
+          >
+            {"(416)"}-335-3353
+          </a>
+        </li>
+        <li>
+          Fax: {"(416)"} 335-7967 
+        </li> 
+        <li>
+        Planning & Building Consultants <br/>
+        850 Tapscott Road, Suite 51 <br/>
+        Toronto, ON M1X 1N4 
+        </li>
+      </ul>
+    </section>
+  )
+}
