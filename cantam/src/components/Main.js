0efa68bed8811c22f9ae08f962c6f1120a1509ce
ConstantMotion1 <constan.fernando@hotmail.com>
2022-12-28 21:38:35 -0500

    added images to project page

--- cantam/src/components/Main.js
+++ cantam/src/components/Main.js
@@ -7,23 +7,31 @@ import NotFound from "./pages/NotFound";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Careers from "./pages/Careers";
+import Projects from "./pages/Projects";
+
+
 
 import { Route, Routes } from "react-router-dom";
 
 export default function Main() {
 
+  const style = {
+    paddingTop : "7rem"
+  }
   return (
     <div>
       <Navbar />
-      <>
+      <div style={style}>
         <Routes>
           <Route path="/" element={<HeroImg />} />
           <Route path="/home" element={<Homepage />} />
           <Route path="/about" element={<About />} />
           <Route path="/careers" element = {<Careers />} />
           <Route path="/contact" element = {<Contact />} />
+          <Route path="/project" element = {<Projects />} />
+          <Route path="/*" element = {<NotFound />} />
         </Routes>
-      </>
+      </div>
       <Footer />
     </div>
   );
